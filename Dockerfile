90b0b83284e2a089b0c2b5ff01215a4ab1350aee
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-02-06 13:33:29 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.8.3
+LABEL protos="0.0.1" \
+      protos.installer.metadata.description="This applications provides a proxy for doing Protos application development" \
+      protos.installer.metadata.capabilities="ResourceProvider,ResourceConsumer,InternetAccess,GetInformation" \
+      protos.installer.metadata.requires="dns" \
+      protos.installer.metadata.provides="certificate" \
+      protos.installer.metadata.name="protosproxy"
+ADD . "/go/src/protosproxy/"
+WORKDIR "/go/src/protosproxy/"
+#RUN go get github.com/jpillora/go-tcp-proxy/cmd/tcp-proxy
+RUN go get github.com/nustiueudinastea/protoslib-go
+CMD go run protosproxy.go
---
+++ protosproxy.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	protos "github.com/nustiueudinastea/protoslib-go"
+)
+
+type Prox struct {
+	target *url.URL
+	proxy  *httputil.ReverseProxy
+	appID  string
+}
+
+func New(target string) *Prox {
+	url, _ := url.Parse(target)
+
+	return &Prox{target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
+}
+
+func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-GoProxy", "GoProxy")
+	r.Header.Set("Appid", p.appID)
+	p.proxy.ServeHTTP(w, r)
+}
+
+func main() {
+	const (
+		defaultPort        = ":9999"
+		defaultPortUsage   = "default server port, ':9999'"
+		defaultTarget      = "http://protos:8080"
+		defaultTargetUsage = "default redirect url, 'http://protos:8080'"
+	)
+
+	// flags
+	port := flag.String("port", defaultPort, defaultPortUsage)
+	url := flag.String("url", defaultTarget, defaultTargetUsage)
+
+	flag.Parse()
+
+	fmt.Printf("server will run on : %s\n", *port)
+	fmt.Printf("redirecting to :%s\n", *url)
+
+	// proxy
+	proxy := New(*url)
+
+	appID, err := protos.GetAppID()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	proxy.appID = appID
+
+	// server
+	http.HandleFunc("/", proxy.handle)
+	http.ListenAndServe(*port, nil)
+}
